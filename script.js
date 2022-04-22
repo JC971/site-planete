20202320962be6a12c78aaf9908706c63c897eff
JC971 <99178903+JC971@users.noreply.github.com>
2022-04-22 15:57:00 +0200

    javascript texte

---
+++ script.js
@@ -0,0 +1,19 @@
+const target = document.getElementById('target');
+let array = ['simple', 'clear', 'smart' , 'strong'];
+let = wordIndex = 0;
+let letterIndex = 0;
+
+const createLetter =() =>{
+    const letter = document.createElement('span');
+    target.appendChild(letter);
+    letter.classList.add('letter');
+    letter.style.opacity ='0';
+    letter.style.animation = ' anim 5s ease forward';
+    letter.textContent = array[wordIndex][letterIndex];
+
+    setTimeout(()=>{
+        letter.remove();
+
+    }, 200)
+}
+
